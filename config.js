4b29e9cdaeefb410f8b7e79fc3854b3c6f3cd648
Xiaowu Chen <five3@163.com>
2017-04-17 10:25:36 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+{
+  "template": {
+    "path" : "D:/tunicorn-web/framework-template",
+    "exclude" : {
+      "file_path" : [],
+      "file_name" : ["config.json", "make_project.py"],
+      "ext_name" : ["css", "woff2"],
+      "file_type" : [],
+      "regex" : []
+    },
+    "include" : {
+      "file_path" : [],
+      "file_name" : []
+    }
+  },
+  "store":{
+    "dir_path" : "e:/test"
+  },
+  "data" : {
+    "project_name":"NewJAVA",
+    "project_prefix":"newjava"
+  }
+}
